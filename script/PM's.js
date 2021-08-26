14c624f4dbe8d2fc36108c60d101e6c6f6f2eb9d
joao-java <joaomelo_9@hotmail.com>
2021-08-26 09:36:56 -0300

    26-08-21.2

--- script/PM's.js
+++ script/PM's.js
@@ -1,20 +1,18 @@
 
 
 //Background automatico 
-let diaMesC = document.querySelector(".diaMes").children
-let diaAtual = new Date().getDate()
+let diaMesC = document.querySelector(".diaMes").children, diaAtual = new Date().getDate()
 
 for(i = 0; i < (diaAtual-1) ; i++){
     let contar = (i + 0) 
     let comecar = diaMesC[contar]
-    console.log(comecar)
     if(comecar){
         comecar.style.background = "#000"
         comecar.style.color = "#fff"
     }
 }
-//Alert 
 
+//Alert 
 alert("Estou em casa pessoal, porem ainda atualizo o calendario" + "\n" + "ASS: João Melo/Moiado")
 
 
